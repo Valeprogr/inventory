6c81d816867dca0be0bd15a535b14eb5014aa702
valentina vittoria <valeviit@icloud.com>
2023-03-28 11:39:29 +0200

    create routes and route

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import ProductsList from "./pages/ProductsList";
+import ProductInfo from "./pages/ProductInfo";
+import NavBar from "./components/NavBar";
 function App() {
+  
   return (
     <>
-      <h1>Hello world!</h1>
+      <NavBar />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/products" element={<ProductsList />} />
+          <Route path="/product" element={<ProductInfo />} />
+        </Routes>
+      </div>
     </>
   );
 }
