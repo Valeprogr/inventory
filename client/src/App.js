26a0dd1ebc1f154349f6bd2b95a694ae8f72bacd
valentina vittoria <valeviit@icloud.com>
2023-03-28 11:55:34 +0200

    install tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <NavBar />
-      <div>
+      <div className="mb-4">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
