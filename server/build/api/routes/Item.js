48970aff8ac19a94f4cb927d3111f2c7f4505d51
valentina vittoria <valeviit@icloud.com>
2023-04-12 19:45:12 +0200

    compile a Production Build

---
+++ server/build/api/routes/Item.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+const express_1 = __importDefault(require("express"));
+const Item_1 = __importDefault(require("../controllers/Item"));
+const router = express_1.default.Router();
+router.get("/get", Item_1.default.getAllItems);
+router.get("/get/:itemArticle", Item_1.default.findItem);
+router.post("/create", Item_1.default.createItem);
+router.patch("/update/:itemArticle", Item_1.default.updateItem);
+router.delete("/delete/:itemArticle", Item_1.default.deleteItem);
+module.exports = router;
